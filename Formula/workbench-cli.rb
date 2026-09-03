377e251e36db73d54877a5c66f31ff43abe187fa
Pigeon <pigeon@r26d.com>
2026-09-02 22:32:10 -0500

    Update workbench-cli to 2026.9.1

--- Formula/workbench-cli.rb
+++ Formula/workbench-cli.rb
@@ -1,15 +1,15 @@
 class WorkbenchCli < Formula
   desc "Workbench command-line client"
   homepage "https://workbench.r26d.dev"
-  version "2026.8.16"
+  version "2026.9.1"
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://updates.r26d.dev/apps/workbench/releases/2026.8.16/macos/arm64/tarball/workbench-cli-2026.8.15-macos-arm64.tar.gz"
-    sha256 "2f567da0e1beeefb804c7dbc9bb3b8bde409e1f2c4534dfb360c48d969cb7cd1"
+    url "https://updates.r26d.dev/apps/workbench/releases/2026.9.1/macos/arm64/tarball/workbench-cli-2026.9.1-macos-arm64.tar.gz"
+    sha256 "17f08b6374662debca273279b87e988fde5f7c6a33a395ee2f985ede0de12ffc"
   else
-    url "https://updates.r26d.dev/apps/workbench/releases/2026.8.16/macos/amd64/tarball/workbench-cli-2026.8.15-macos-amd64.tar.gz"
-    sha256 "6322ab28bd254926c9ef2b3ade9aa733f6d43dd79cf08c562346b191c63bf2fe"
+    url "https://updates.r26d.dev/apps/workbench/releases/2026.9.1/macos/amd64/tarball/workbench-cli-2026.9.1-macos-amd64.tar.gz"
+    sha256 "4db68468d94af4e7d16976895e6ea5f63a3d17b814bd5294f175fbb4471d23eb"
   end
 
   def install
